64132e1029eccc7ce7d9c77b455647b2853d1d21
yangbin <bin_yang@gzhaoku.com>
2017-09-30 09:19:02 +0800

    commit

---
+++ js/index/index.js
@@ -0,0 +1,19 @@
+$(function () {
+    // iscrool
+    $('.wrapper').navbarscroll();
+
+    // swiper
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        loop : true,
+        autoplay : 3000,
+        onInit: function(swiper){ //Swiper2.x的初始化是onFirstInit
+            swiperAnimateCache(swiper); //隐藏动画元素
+            swiperAnimate(swiper); //初始化完成开始动画
+        },
+        onSlideChangeEnd: function(swiper){
+            swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
+        }
+    });
+})
