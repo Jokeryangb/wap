852f856f595c31e04941c6522422ed43641e7850
yangbin <bin_yang@gzhaoku.com>
2017-10-11 14:36:32 +0800

    加入我们

--- js/layout.js
+++ js/layout.js
@@ -3,17 +3,21 @@ $(function () {
     $('.wrapper').navbarscroll();
 
     // swiper
-    var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        paginationClickable: true,
-        loop : true,
-        autoplay : 3000,
-        onInit: function(swiper){ //Swiper2.x的初始化是onFirstInit
-            swiperAnimateCache(swiper); //隐藏动画元素
-            swiperAnimate(swiper); //初始化完成开始动画
-        },
-        onSlideChangeEnd: function(swiper){
-            swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
-        }
-    });
+    var flag = $('.swiper-container').length
+    if (flag > 0) {
+        var swiper = new Swiper('.swiper-container', {
+            pagination: '.swiper-pagination',
+            paginationClickable: true,
+            loop : true,
+            autoplay : 3000,
+            onInit: function(swiper){ //Swiper2.x的初始化是onFirstInit
+                swiperAnimateCache(swiper); //隐藏动画元素
+                swiperAnimate(swiper); //初始化完成开始动画
+            },
+            onSlideChangeEnd: function(swiper){
+                swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
+            }
+        });
+    }
+
 })
